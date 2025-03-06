00cbbe7bdf94d512233fbb0e26cbe47b2580c81e
bartelsx <bartelsx@gmail.com>
2025-03-06 14:01:40 +0100

    empty cmake project
    empty cmake project

---
+++ Connect4Pytorch/Connect4Pytorch/Connect4Pytorch.cpp
@@ -0,0 +1,12 @@
+﻿// Connect4Pytorch.cpp : Defines the entry point for the application.
+//
+
+#include "Connect4Pytorch.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
